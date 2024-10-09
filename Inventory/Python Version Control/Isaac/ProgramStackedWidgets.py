d09d58bb6560a8ac4856388da939764c217ddfda
Isaac <57078036+MrMiffs@users.noreply.github.com>
2024-10-09 17:24:52 -0600

    Added constant Home button at top of screen

--- Inventory/Python Version Control/Isaac/ProgramStackedWidgets.py
+++ Inventory/Python Version Control/Isaac/ProgramStackedWidgets.py
@@ -4,9 +4,10 @@ from PySide6.QtCore import QRect
 from PySide6.QtGui import QFontMetrics
 from InventoryScreen import InventoryScreen
 
+
 # Home Screen Class
 class HomeScreen(QWidget):
-    def __init__(self, switch_to_inventory, switch_to_login):
+    def __init__(self, switch_to_inventory, switch_to_sales):
         super().__init__()
         layout = QVBoxLayout(self)
         self.label = QLabel("Home Screen", self)
@@ -17,63 +18,70 @@ class HomeScreen(QWidget):
         self.inventoryButton.clicked.connect(switch_to_inventory)
         layout.addWidget(self.inventoryButton)
 
-        self.loginButton = QPushButton("Go to Login", self)
-        self.loginButton.clicked.connect(switch_to_login)
+        self.loginButton = QPushButton("Go to Sales", self)
+        self.loginButton.clicked.connect(switch_to_sales)
         layout.addWidget(self.loginButton)
 
         self.setLayout(layout)
 
-# Login Screen Class
-class LoginScreen(QWidget):
+
+# Sales Screen Class
+class SalesScreen(QWidget):
     def __init__(self):
         super().__init__()
         layout = QVBoxLayout(self)
-        self.label = QLabel("Login Screen", self)
+        self.label = QLabel("Sales Screen", self)
         layout.addWidget(self.label)
         self.setLayout(layout)
+
 
 # Main Widget Class
 class MainWidget(QWidget):
     def __init__(self):
         super().__init__()
 
+        # Create a button that jumps back to the Home Screen
+        self.homeButton = QPushButton("Home")
+        self.homeButton.clicked.connect(self.showHomeScreen)  # Connect to the home screen method
+
+        # Create a QVBoxLayout to hold the button and stacked widget
+        self.mainLayout = QVBoxLayout()
+        self.mainLayout.addWidget(self.homeButton)
+
+
         # Create a QStackedWidget to manage different screens
         self.stackedWidget = QStackedWidget()
 
-        # Create instances of your screens
-        self.homeScreen = HomeScreen(self.showInventoryScreen, self.showLoginScreen)
+        # Create screens
+        self.homeScreen = HomeScreen(self.showInventoryScreen, self.showSalesScreen)
         self.inventoryScreen = InventoryScreen()
-        self.loginScreen = LoginScreen()
+        self.salesScreen = SalesScreen()
 
         # Add screens to the QStackedWidget
         self.stackedWidget.addWidget(self.homeScreen)      # Index 0
         self.stackedWidget.addWidget(self.inventoryScreen) # Index 1
-        self.stackedWidget.addWidget(self.loginScreen)     # Index 2
+        self.stackedWidget.addWidget(self.salesScreen)     # Index 2
 
         # Set the initial screen (Home Screen)
         self.stackedWidget.setCurrentWidget(self.homeScreen)
 
-        # Create a main layout to hold the stacked widget
-        mainLayout = QHBoxLayout()
-        mainLayout.addWidget(self.stackedWidget)
-        self.setLayout(mainLayout)
+        # Add the stacked widget to the main layout
+        self.mainLayout.addWidget(self.stackedWidget)
+        self.setLayout(self.mainLayout)
+
 
     def showHomeScreen(self):
-        """ Switch to the Home Screen """
         self.stackedWidget.setCurrentWidget(self.homeScreen)
 
     def showInventoryScreen(self):
-        """ Switch to the Inventory Screen """
         self.stackedWidget.setCurrentWidget(self.inventoryScreen)
 
-    def showLoginScreen(self):
-        """ Switch to the Login Screen """
-        self.stackedWidget.setCurrentWidget(self.loginScreen)
+    def showSalesScreen(self):
+        self.stackedWidget.setCurrentWidget(self.salesScreen)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    # Now the main widget is a QWidget
     mainWidget = MainWidget()
     mainWidget.resize(800, 600)
     mainWidget.show()
