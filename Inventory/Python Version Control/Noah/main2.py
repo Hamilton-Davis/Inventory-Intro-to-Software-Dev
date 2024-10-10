04438316d2277050e711492b1cd26a4c90e98b05
Noah Storms <70107291+storms421@users.noreply.github.com>
2024-10-10 01:15:47 -0600

    Update main2.py

--- Inventory/Python Version Control/Noah/main2.py
+++ Inventory/Python Version Control/Noah/main2.py
@@ -1,33 +1,32 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 from ui import CentralWidget  # Import CentralWidget from ui.py
+from login import LoginWindow  # Import LoginWindow from login.py
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
-        # Set the window title
-        self.setWindowTitle("Inventory Management")
-        # Resize the main window
-        self.resize(1080, 800)
+        super().__init__()  # Call parent QMainWindow constructor
+        self.setWindowTitle("Inventory Management")  # Set window title
+        self.resize(1080, 800)  # Set main window size
+
         # Create an instance of CentralWidget and pass the exit method as a callback
         self.central_widget = CentralWidget(self, self.exit_application)
-        self.setCentralWidget(self.central_widget)
+        self.setCentralWidget(self.central_widget)  # Set central widget for main window
 
-    @staticmethod  # Indicates this method not dependent on instance state
-    # Function to exit the application
+    @staticmethod
     def exit_application():
         print("Closing Application")
         QApplication.quit()
 
 
 if __name__ == "__main__":
-    # Create the QApplication instance
     app = QApplication(sys.argv)
-    # Create an instance of MainWindow
-    window = MainWindow()
-    # Show the main window
-    window.show()
-    # Start the application's event loop
-    sys.exit(app.exec())
 
+    # Create an instance of LoginWindow
+    login_window = LoginWindow()
+    if login_window.exec():  # Will block until login window is closed
+        main_window = MainWindow()
+        main_window.show()  # Show main window
+
+    sys.exit(app.exec())
