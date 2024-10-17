577c4206ebdb15a1035723407521a01966e1686a
Isaac <57078036+MrMiffs@users.noreply.github.com>
2024-10-16 20:37:52 -0600

    Created get_newest_file(folderPath)
    Takes path to folder of Excel files and returns the path to the file with the most recent date in the file name

--- Inventory/Python Version Control/Hamilton/Sandbox.py
+++ Inventory/Python Version Control/Hamilton/Sandbox.py
@@ -1,11 +1,31 @@
 import datetime
 import xlwt
 import os
-from openpyxl import Workbook as wb
+from openpyxl import Workbook
 from openpyxl.reader.excel import load_workbook
+from pathlib import Path
+from dateutil.parser import parse
+
+#Gets path to most recent .xlsx file in directory
+def get_newest_file(folderPath):
+    files = []
+    for filePath in Path.iterdir(folderPath):
+        if filePath.suffix == '.xlsx':
+            try:
+                date = parse(filePath.stem, fuzzy=True) # Pulls first 3-integer date (e.g. xx-xx-xxxx) found in file name (stem)
+                file = (date, filePath)
+                files.append(file)
+            except ValueError: # Could not parse 3-integer date from file name
+                pass
+
+    files.sort(reverse=True) # Sort in descending order, newest dates first
+    return files[0][1] # Returns filePath of newest file
 
 
-folderLocation = "C:\\Users\\mirac\\OneDrive\\Desktop\\Inventory-Intro-to-Software-Dev-main\\Inventory-Intro-to-Software-Dev-main\\Inventory Files\\Excel Sheets\\Excel Sheet"
+#folderLocation = "C:\\Users\\mirac\\OneDrive\\Desktop\\Inventory-Intro-to-Software-Dev-main\\Inventory-Intro-to-Software-Dev-main\\Inventory Files\\Excel Sheets\\Excel Sheet"
+folderLocation = Path(__file__).parent.parent.parent.absolute() / "Excel Sheets" # Absolute path to Inventory folder:
+# Goes up from script file location to parent "Hamilton", parent "Python Version Control", parent "Inventory", appends "Excel Sheets"
+
 
 # Finds Current Date, Allows for File Management
 current_time = datetime.datetime.now()
@@ -17,13 +37,15 @@ date = month+day+year
 
 # Defines file location for new file
 excelName = "Inventory-"+ date + ".xlsx"
-newFileLocation = folderLocation + '\\' + excelName
+newFileLocation = folderLocation / excelName
+
 print(newFileLocation)
 
+
 # Default File, It must have this on the file
-for filename in os.listdir(folderLocation):
-    if filename.endswith(".xlsx"):
-        copyPath = os.path.join(folderLocation, filename)
+for filename in Path.iterdir(folderLocation):
+    if filename.suffix == ".xlsx":
+        copyPath = folderLocation / filename
         print(filename)
     else:
         wb = Workbook()
@@ -38,4 +60,5 @@ for filename in os.listdir(folderLocation):
         ws['H1'] = "Entry Date"
 
 
-wb.save(filename)
+#wb.save(filename) # This saves file to current directory of script file, not to newFileLocation
+wb.save(newFileLocation)
